1d1e17cb2c2eec1973f23fce49dfd43e913c9af3
Mattfava <61483306+Mattfava@users.noreply.github.com>
2020-04-16 20:36:46 -0400

    Add files via upload

--- project.js
+++ project.js
@@ -1,18 +1,4 @@
 
-//var key = "rbseysDYVUGVCe2W";
-//var secretKey = "6RjQdBqsG6pzwX4p";
-//var userName = "fava247";
-//var password = "bootcamp247";
-
-
-
-
-
-
-
-
-
-
 //first four are algebra 1
 //second three are geometry
 //array of math youtube links
@@ -169,6 +155,32 @@ function videoPlay7() {
     });
 }
 
+var APIKey = "6d58d3b18d5df4c85f443a943955e08c";
 
+var queryURL2 = "https://api.openweathermap.org/data/2.5/weather?q=New York City,US&appid=" + APIKey;
 
+$.ajax({
+  url: queryURL2,
+  method: "GET"
+}).then(function(response) {
+
+  console.log(response);
+  var Temp = response.main.temp;
+  var fTemp = Temp*9/5-459.67;
+  var fTemp2 = Math.trunc(fTemp);
+  console.log(fTemp2);
+  var screenTemp = document.getElementById("temperature");
+  var stringTemp = "Temperature: "+fTemp2+" F";
+  screenTemp.innerHTML = stringTemp;
+  var windSpeed = response.wind.speed;
+  var screenWindSpeed = document.getElementById("wind-speed");
+  var stringWind = "Wind Speed: "+windSpeed+" MPH";
+  screenWindSpeed.innerHTML = stringWind;
+  var feels = response.main.feels_like;
+  var fFeels = feels*9/5-459.67;
+  var fFeels2 = Math.trunc(fFeels);
+  var screenFeels = document.getElementById("feels");
+  var stringFeels = "Feels Like: "+fFeels2+" F";
+  screenFeels.innerHTML = stringFeels;
+})
 
